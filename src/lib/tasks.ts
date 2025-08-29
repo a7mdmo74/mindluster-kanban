e356b10e0b6a5a9925a68d89f8bda4c96fb652cf
Ahmed Muhammad <a7mdmo74@gmail.com>
2025-08-29 20:38:11 +0300

    update env to deploy

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Task, CreateTaskInput, UpdateTaskInput } from "@/lib/typing";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
 });
 
 export const getTasks = async (): Promise<Task[]> => {
